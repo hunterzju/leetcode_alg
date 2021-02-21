df3cd3dbef68511c24495d37dd8888c88056fc7a
hunterzju <313821078@qq.com>
2021-02-21 22:28:28 +0800

    WIP(offer10_2): add sol for off10_2

--- offer10_2.py
+++ offer10_2.py
@@ -7,7 +7,22 @@ class Solution:
         else:
             return (self.numWays(n-1) + self.numWays(n-2)) % 1000000007
 
+    '''
+    jump 0: 1
+    jump 1: 1
+    jump 2: jump[0] + jump[1]
+    '''
+    def numWays_bp(self, n: int) -> int:
+        if n <= 1:
+            return 1
+        bp_list = [-1 for i in range(0,n+1)]
+        bp_list[0] = 1
+        bp_list[1] = 1
+        for i in range(2,n+1):
+            bp_list[i] = (bp_list[i-1] + bp_list[i-2]) % 1000000007
+        return bp_list[n]
+
 if __name__ == "__main__":
     sol = Solution()
-    res = sol.numWays(100)
+    res = sol.numWays_bp(2)
     print(res)
